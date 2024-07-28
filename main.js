2a2726430f9272d87590119024eccb7d5b554814
AastikP <168633857+AastikP@users.noreply.github.com>
2024-07-28 23:23:48 +0530

    test 1
    first

---
+++ main.js
@@ -0,0 +1,26 @@
+/*        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyDeQHyfHFIYREWLjkiGhHi_NRJVb-1V65Y",
+          authDomain: "card-saver-9231b.firebaseapp.com",
+          projectId: "card-saver-9231b",
+          storageBucket: "card-saver-9231b.appspot.com",
+          messagingSenderId: "246096868175",
+          appId: "1:246096868175:web:02f6f3df03b3d5f1f14b3a",
+          measurementId: "G-MBT19HKZJQ"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+*/
+function test()
+{
+  document.getElementById("fing_ani").play();
+}
